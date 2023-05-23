e4747c3539a20fc1e63cdc9d36aa708fc23733ce
Zakaria Ben <75684862+zaka-rgb@users.noreply.github.com>
2023-05-23 23:19:04 +0200

    Add files via upload

---
+++ les operations/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ int nb1=0 ,nb2=0,resultat=0;
+ printf("entrez deux entiers \n");
+  scanf("%d=",&nb1);
+scanf("%d=",&nb2);
+resultat=nb1+nb2;
+printf("%d+%d=%d",nb1,nb2,resultat);
+    return 0;
+}
+
